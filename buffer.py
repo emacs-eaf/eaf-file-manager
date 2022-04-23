fd8f03b16f154405ab3a85026f52b63527805de5
Andy Stewart <lazycat.manatee@gmail.com>
2022-04-23 08:03:07 +0800

    Add Dir prefix in title.

--- buffer.py
+++ buffer.py
@@ -321,7 +321,7 @@ class AppBuffer(BrowserBuffer):
         self.monitor_current_dir()
 
         eval_in_emacs('eaf--change-default-directory', [self.buffer_id, dir])
-        self.change_title(os.path.basename(dir))
+        self.change_title("Dir [{}]".format(os.path.basename(dir)))
         
         self.file_infos = self.get_file_infos(dir)
 
